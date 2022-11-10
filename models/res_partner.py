58c93ae5adbb4e5f9b1bc681b3f3878d6121a93b
99070717 <camilo.beltran@vemesco.com>
2022-11-10 22:59:18 +0000

    ADD Validations for import data

--- models/res_partner.py
+++ models/res_partner.py
@@ -24,8 +24,15 @@ class ResPartner(models.Model):
 # define vat and email_edi with unique fields
     @api.constrains('vat','email_edi')
     def _check_vat_unique(self):
+        is_extra_funcion = self.env.user.company_id.extra_function
+        email_is_required = self.env.user.company_id.email_is_required
+        regime_type_is_required = self.env.user.company_id.regime_type_is_required
+        municipality_is_required = self.env.user.company_id.municipality_is_required
+        document_type_is_required = self.env.user.company_id.document_type_is_required
+        liability_is_required = self.env.user.company_id.liability_is_required
+        vat_is_required = self.env.user.company_id.vat_is_required
+
         for record in self:
-            is_extra_funcion = self.env.user.company_id.extra_function
             if record.vat:
                 #ADD validations to VAT
                 vat_only_nums = (record.vat).replace('-','')
@@ -34,16 +41,32 @@ class ResPartner(models.Model):
                         _logger.info("Correct VAT format")
                     else:
                         raise ValidationError(_('The "'+ record.vat +'" VAT does not comply with the correct format.\n' + 'Correct Nit format: xxxxxxxxx-x  where x represents an integer digit.\n' +'If your document type is other, the correct format are only numbers.'))
-                
-            if record.vat and is_extra_funcion:
-                partner_objs = record.env['res.partner'].search([('vat','=',record.vat),('id','!=',record.id)])
-                if record.get_partner_list(partner_objs) and record.vat:
-                    raise ValidationError(_('The vat ' +record.vat+' is already exist in the following records:' + '\n' + record.get_partner_list(partner_objs)))
+            if is_extra_funcion:
 
-            if record.email_edi and is_extra_funcion:
-                partner_objs = record.env['res.partner'].search([('email_edi','=',record.email_edi),('id','!=',record.id)])
-                if record.get_partner_list(partner_objs):
-                    raise ValidationError(_('The Email for invoicing ' +record.email_edi+' is already exist in the following records:' + '\n' + record.get_partner_list(partner_objs)))
+                if record.vat:
+                    partner_objs = record.env['res.partner'].search([('vat','=',record.vat),('id','!=',record.id)])
+                    if record.get_partner_list(partner_objs) and record.vat:
+                        raise ValidationError(_('The vat ' +record.vat+' is already exist in the following records:' + '\n' + record.get_partner_list(partner_objs)))
+
+                if record.email_edi:
+                    partner_objs = record.env['res.partner'].search([('email_edi','=',record.email_edi),('id','!=',record.id)])
+                    if record.get_partner_list(partner_objs):
+                        raise ValidationError(_('The Email for invoicing ' +record.email_edi+' is already exist in the following records:' + '\n' + record.get_partner_list(partner_objs)))
+
+                #validations for import data 
+                required_fields = {
+                    'vat' : [record.vat,vat_is_required,'vat'],
+                    'l10n_co_document_type' : [record.l10n_co_document_type,document_type_is_required,'Document type'],
+                    'type_regime_id' : [record.type_regime_id,regime_type_is_required,'Regime type'],
+                    'type_liability_id' : [record.type_liability_id,liability_is_required,'Liability type'],
+                    'municipality_id' : [record.municipality_id,municipality_is_required,'Municipality id'],
+                    'email_edi' : [record.email_edi,email_is_required,'email for invoicing']
+                }
+                for rec in required_fields:
+                    if not required_fields[rec][0] and required_fields[rec][1]:    
+                            partner_objs = record.env['res.partner'].search([(rec,'=',False),('id','!=',record.id)])
+                            if record.get_partner_list(partner_objs):
+                                raise ValidationError(_('Please Add '+required_fields[rec][2]))
     
     #Define obligatories fields to Electronic Invoicing
     @api.multi
